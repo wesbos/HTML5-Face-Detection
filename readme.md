dc1a135f2685a6528985bddc0ac2d45c74f4e863
Wes Bos <wesbos@gmail.com>
2012-03-05 16:57:57 -0500

    Changed everything over to work via web RTC realtime webcam

--- readme.md
+++ readme.md
@@ -1,5 +1,22 @@
 This demo uses the CCV Javascript Face Detection Library.
 
+-------------------------------------------------------------------------------
+To start run App.start()  in your console.
+
+You can also pass it a few different effects:
+* App.start('glasses'); // Goofy Glasses example
+* App.start('green'); // Green Screen effect (there is a hidden max and min sliders you can use to adjust the thresholds)
+* App.start('hipster'); // simple pixel manipulation example
+* App.start('blur'); // blur example
+
+Requires a browser that supports getUserMedia. This demo will be updated with opera support soon but currently uses chrome canary
+
+Start Google Chrome Canary with open -a Google\ Chrome\ Canary --args --enable-media-stream  OR enable the flag in about:flags
+
+--------------------------------------------------------------------------------
+
+Original links with a stored local video:
+
 Demo Video: http://www.youtube.com/watch?v=YYES9Qd094o&hd=1
 
 Working Demo : http://wesbos.com/demos/html5-face-detection/ 
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,40 +1,124 @@
-// Setup Video
-var		video = document.querySelector('video'),
-		time_dump = document.getElementById("elapsed_time"),
-		glasses = new Image(),
-		canvas = document.getElementById("output"),
-		ctx = canvas.getContext("2d");
-		glasses.src = "i/glasses.png";
+/** Start Google Chrome Canary with open -a Google\ Chrome\ Canary --args --enable-media-stream  OR enable the flag in about:flags **/
 
-function html5glasses() {
-	// Start the clock 
-	var elapsed_time = (new Date()).getTime();
+var App = {
 
-	// Draw the video to canvas
-	ctx.drawImage(video, 0, 0, video.width, video.height, 0, 0, canvas.width, canvas.height);
+	// Run if we do have camera support
+	successCallback : function(stream) {
+        console.log('yeah! camera support!');
+        App.video.src = window.webkitURL ? window.webkitURL.createObjectURL(stream) : stream;
+    },
 
-	// use the face detection library to find the face
-	var comp = ccv.detect_objects({ "canvas" : (ccv.pre(canvas)),
-									"cascade" : cascade,
-									"interval" : 5,
-									"min_neighbors" : 1 });
+	// run if we dont have camera support
+	errorCallback : function(error) {
+		alert('An error occurred while trying to get camera access (Your browser probably doesnt support getUserMedia() ): ' + error.code);
+		return;
+	},
 
-	// Stop the clock
-	time_dump.innerHTML = "Process time : " + ((new Date()).getTime() - elapsed_time).toString() + "ms";
 
-	// Draw glasses on everyone!
-	for (var i = 0; i < comp.length; i++) {
-		ctx.drawImage(glasses, comp[i].x, comp[i].y,comp[i].width, comp[i].height);
+	drawToCanvas : function(effect) {
+		var video = App.video,
+			ctx = App.ctx,
+			canvas = App.canvas,
+			i;
+
+			ctx.drawImage(video, 0, 0, 520,426);
+
+			App.pixels = ctx.getImageData(0,0,canvas.width,canvas.height);
+
+		// Hipstergram!
+		
+		if (effect === 'hipster') {
+
+			for (i = 0; i < App.pixels.data.length; i=i+4) {
+				App.pixels.data[i + 0] = App.pixels.data[i + 0] * 3 ;
+				App.pixels.data[i + 1] = App.pixels.data[i + 1] * 2;
+				App.pixels.data[i + 2] = App.pixels.data[i + 2] - 10;
+			}
+
+			ctx.putImageData(App.pixels,0,0);
+
+		}
+
+		// Blur!
+
+		else if (effect === 'blur') {
+			stackBlurCanvasRGBA('output',0,0,515,426,20);
+		}
+
+		// Green Screen
+
+		else if (effect === 'greenscreen') {
+				
+					/* Selectors */
+					var rmin = $('#red input.min').val();
+					var gmin = $('#green input.min').val();
+					var bmin = $('#blue input.min').val();
+					var rmax = $('#red input.max').val();
+					var gmax = $('#green input.max').val();
+					var bmax = $('#blue input.max').val();
+
+					// console.log(rmin,gmin,bmin,rmax,gmax,bmax);
+					
+					for (i = 0; i < App.pixels.data.length; i=i+4) {
+									red = App.pixels.data[i + 0];
+									green = App.pixels.data[i + 1];
+									blue = App.pixels.data[i + 2];
+									alpha = App.pixels.data[i + 3];
+
+									if (red >= rmin && green >= gmin && blue >= bmin && red <= rmax && green <= gmax && blue <= bmax ) {
+										App.pixels.data[i + 3] = 0;
+									}
+					}
+
+					ctx.putImageData(App.pixels,0,0);
+
+		}
+		else if(effect === 'glasses') {
+			var comp = ccv.detect_objects({ "canvas" : (App.canvas),
+											"cascade" : cascade,
+											"interval" : 5,
+											"min_neighbors" : 1 });
+
+			// Draw glasses on everyone!
+			for (i = 0; i < comp.length; i++) {
+				ctx.drawImage(App.glasses, comp[i].x, comp[i].y,comp[i].width, comp[i].height);
+			}
+						
+		}
+	
+					
+	},
+
+	start : function(effect) {
+		if(App.playing) { clearInterval(App.playing); }
+		App.playing = setInterval(function() {
+			App.drawToCanvas(effect);
+		},50);
 	}
-}
+};
 
-/* Events */ 
+App.init = function() {
+	// Prep the document
+	App.video = document.querySelector('video');
+	
+	App.glasses = new Image();
+	App.glasses.src = "i/glasses.png";
 
-video.addEventListener('play', function() {
-	vidInterval = setInterval(html5glasses,200); 
-});
+	App.canvas = document.querySelector("#output");
+	App.ctx = this.canvas.getContext("2d");
 
-video.addEventListener('ended', function() {
-	clearInterval(vidInterval);
-	time_dump.innerHTML = "finished";
-});
+
+	// Finally Check if we can run this puppy and go!
+	if (navigator.webkitGetUserMedia) {
+		navigator.webkitGetUserMedia('video', App.successCallback, App.errorCallback);
+	}
+
+	App.start();
+
+};
+
+
+document.addEventListener("DOMContentLoaded", function() {
+	console.log('ready!');
+	App.init();
+}, false);
